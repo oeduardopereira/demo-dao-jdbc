c1a6c7c74d9c4de6a3730c366c78c75d0207a0df
eduardo pereira filho <eduardopereirafilho6@gmail.com>
2022-11-14 09:31:42 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,5 +15,7 @@ public class Program {
 
         Seller s1 = new Seller("Eduardo", 1, 3000.0, new Date(), "edu@gmail.com", dp);
         System.out.println(s1);
+
+        SellerDAO sellerDAO =  DaoFactory.createSellerDAO();
     }
 }
